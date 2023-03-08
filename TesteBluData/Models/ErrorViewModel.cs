2096e883fe89f8144da23cc5da0aaa9296a31f47
Thierry Stuhler <thierrystuhler@gmail.com>
2023-03-08 17:13:44 -0300

    Projeto Criado

---
+++ TesteBluData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteBluData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
