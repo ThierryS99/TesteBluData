1381fdd85149f69572cc09fb73b6a443ab673584
Thierry Stuhler <thierrystuhler@gmail.com>
2023-03-08 23:21:16 -0300

    Adicionado exemplo de tabela de consulta.

--- TesteBluData/Models/FornecedorPF.cs
+++ TesteBluData/Models/FornecedorPF.cs
@@ -2,32 +2,31 @@
 
 namespace TesteBluData.Models
 {
-    [Table("FornecedorPf")]
+	[Table("FornecedorPf")]
 
-    public class FornecedorPf
-    {
-        [Column("")]
+	public class FornecedorPf
+	{
+		[Column("")]
+		public string Nome { get; set; }
+		public string CadastroId { get; set; } // CPF OU CNPJ   
+		public string Telefone { get; set; }
+		public string Rg { get; set; }
+		public DateTime Nascimento { get; set; }
+		public string Empresa { get; set; }
+		public DateTime DataCadastro { get; set; }
 
-        public Empresa Empresa { get; set; }
-        public string Nome { get; set; }
-        public string CadastroId { get; set; } // CPF OU CNPJ   
-        public DateTime DataCadastro { get; set; }
-        public string Telefone { get; set; }
-        public string Rg { get; set; }
-        public DateTime Nascimento { get; set; }
+		public FornecedorPf() { }
 
-        public FornecedorPf() { }
-
-        public FornecedorPf(Empresa empresa, string nome, string cadastroId, DateTime dataCadastro, string telefone, string rg, DateTime nascimento) // Caso seja CPF
-        {
-            Empresa = empresa;
-            Nome = nome;
-            CadastroId = cadastroId;
-            Telefone = telefone;
-            DataCadastro = dataCadastro;
-            Telefone = telefone;
-            Rg = rg;
-            Nascimento = nascimento;
-        }
-    }
+		public FornecedorPf(string empresa, string nome, string cadastroId, DateTime dataCadastro, string telefone, string rg, DateTime nascimento) // Caso seja CPF
+		{
+			Empresa = empresa;
+			Nome = nome;
+			CadastroId = cadastroId;
+			Telefone = telefone;
+			DataCadastro = dataCadastro;
+			Telefone = telefone;
+			Rg = rg;
+			Nascimento = nascimento;
+		}
+	}
 }
